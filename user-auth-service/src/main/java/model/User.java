d7ad78f3f4018df5d39d414ff2539fe4b70abd07
Madushani Wijerathna <kalpaniwasana99@gmail.com>
2026-08-10 14:59:45 +0530

    Fixed User model getters/setters and test dependencies

--- user-auth-service/src/main/java/model/User.java
+++ user-auth-service/src/main/java/model/User.java
@@ -2,14 +2,8 @@ package com.realestate.user_auth_service.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Document(collection = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     private String id;
@@ -17,4 +11,58 @@ public class User {
     private String email;
     private String password;
     private String role;
+
+    // හිස් Constructor එක
+    public User() {
+    }
+
+    // සියලු විස්තර සහිත Constructor එක
+    public User(String id, String username, String email, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters සහ Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
